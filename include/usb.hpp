7541bdd32ce25818467651858aaa571e2d3b43e8
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-23 21:26:41 -0500

    include/usb.hpp: Initial header for USB class

---
+++ include/usb.hpp
@@ -0,0 +1,66 @@
+#ifndef OSPREY_ROBOTICS__USB_HPP
+#define OSPREY_ROBOTICS__USB_HPP
+
+#include <libusb-1.0/libusb.h>
+#include <string>
+#include <unordered_map>
+
+namespace osprey_robotics
+{
+    /**
+     * @brief Class to interface with USB devices, primarily USB motors
+     */
+	class USB
+	{
+        public:
+            /**
+             * @brief Construct a new USB object, handles context initialization
+             */
+            USB();
+
+            /**
+             * @brief Destroy the USB object, closes any loaded device handles
+             *        in addition to USB context exit
+             */
+            virtual ~USB();
+
+            /**
+             * @brief Loads USB Device handles by vendor and product IDs
+             * 
+             * @param vid USB Vendor ID
+             * @param pid USB Product ID
+             * @return uint8_t the number of devices loaded
+             */
+            uint8_t loadDevices(uint16_t vid, uint16_t pid);
+
+            /**
+             * @brief Print information on loaded devices
+             */
+            void printDevices();
+
+            /**
+             * @brief Perform a bulk transfer/write to loaded USB device
+             * 
+             * @param serial USB device serial number
+             * @param endpoint USB device endpoint
+             * @param data data to be transferred/written
+             * @param length length/size of the data
+             * @param transferred number of bytes transferred
+             * @param timeout timeout (in milliseconds) this function should wait
+             * @return uint8_t number of bytes transferred
+             */
+            uint8_t bulkWrite(std::string serial,
+                              unsigned char endpoint,
+                              unsigned char* data,
+                              int length,
+                              int* transferred,
+                              unsigned int timeout);
+
+        private:
+            std::unordered_map<std::string, libusb_device_handle*> _devices;
+            libusb_context *_usbContext;
+
+    };
+}
+
+#endif
