f2d4b907dd52660b8eb7ca04d1f8e4e1ad6e10d5
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-27 17:02:47 -0500

    include/joint.hpp: Initial header for Joint class

---
+++ include/joint.hpp
@@ -0,0 +1,102 @@
+/** Copyright 2023 Osprey Robotics - UNF
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*/
+
+#ifndef OSPREY_ROBOTICS__JOINT_HPP
+#define OSPREY_ROBOTICS__JOINT_HPP
+
+#include <cstdint>
+#include <string>
+
+#include "usb.hpp"
+
+// types of actuators
+#define ACTUATOR_TYPE_NONE -1
+#define ACTUATOR_TYPE_MOTOR 0
+#define ACTUATOR_TYPE_LINEAR 1
+
+// Spark Max USB
+#define MOTOR_USB_LEN 24
+#define MOTOR_USB_PID 0xA30E
+#define MOTOR_USB_VID 0x0483
+#define MOTOR_USB_ENDPOINT 0x02
+
+// motor USB serial numbers
+const std::string SERIAL_FRONT_LEFT_1 = "206D33614D43";
+const std::string SERIAL_BACK_LEFT_2 = "2061376C4243";
+const std::string SERIAL_FRONT_RIGHT_3 = "206B336B4E55";
+const std::string SERIAL_BACK_RIGHT_4 = "205A336B4E55";
+const std::string SERIAL_LADDER_DIG = "206A33544D43";
+const std::string SERIAL_LADDER_LIFT = "206C395A5543";
+const std::string SERIAL_DEPOSITION = "205D39515543";
+
+// values in joint.cpp
+// speed of motors range -0.50 to 0.50 big endian
+extern unsigned char SPEED[MOTOR_USB_LEN];
+// activate motors
+extern unsigned char ACTUATE[MOTOR_USB_LEN];
+
+namespace osprey_robotics
+{
+    /**
+     * @brief Class to interface with and represent motors, actuators, and other
+     *        joints connected to the robot 
+     */
+    class Joint
+    {
+        public:
+            std::string name;
+            USB *usb;
+
+            /**
+             * @brief Construct a new Joint object, empty/unused
+             */
+            Joint();
+
+            /**
+             * @brief Construct a new Joint object, primary means to create a
+             *        new joint with a read only id, USB serial number, and
+             *        actuator type.
+             * 
+             * @param id internal joint id, read only after creation
+             * @param serial usb serial number, read only after creation
+             * @param actuatorType type of actuator from DEFINES; motor, actuator, etc
+             */
+            Joint(uint8_t id, std::string serial, uint8_t actuatorType);
+
+            /**
+             * @brief Destroy the Joint object, empty/unused
+             */
+            ~Joint();
+
+            /**
+             * @brief 
+             * 
+             * @param effort 
+             * @param duration 
+             */
+            void actuate(double effort, uint8_t duration);
+
+            /**
+             * @brief Get the internal id of the joint
+             * 
+             * @return uint8_t the internal id number of the joint
+             */
+            uint8_t getId();
+
+            /**
+             * @brief Get the Serial object
+             * 
+             * @return std::string the USB serial number of the joint/device
+             */
+            std::string getSerial();
+
+        private:
+            std::string serial_;
+            uint8_t id_ = 0;
+            uint8_t actuatorType_ = 0;
+    };
+}
+#endif
