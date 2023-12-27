7863cbb301855264827c554c8bff5e26090f1188
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-27 17:49:59 -0500

    include/segment.hpp: Segment class for connecting joints to robot class

---
+++ include/segment.hpp
@@ -0,0 +1,46 @@
+/** Copyright 2023 Osprey Robotics - UNF
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*/
+
+#ifndef OSPREY_ROBOTICS__SEGMENT_HPP
+#define OSPREY_ROBOTICS__SEGMENT_HPP
+
+#include "joint.hpp"
+
+namespace osprey_robotics
+{
+	/**
+	 * @brief Template class that represents segments attached to the robot base
+	 * 
+	 * @tparam T the number of segments attached
+	 */
+	template <int T> class Segment
+	{
+		private:
+
+		public:
+			Joint joints[T];
+
+			/**
+			 * @brief Construct a new Segment object, empty/unused
+			 */
+			Segment() { };
+
+			/**
+			 * @brief Destroy the Segment object, empty/unused
+			 */
+
+			~Segment() { };
+
+			/**
+			 * @brief The size of the 
+			 * 
+			 * @return int 
+			 */
+			int size() const { return T; }
+	};
+}
+
+#endif
