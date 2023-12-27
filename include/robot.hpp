63b7fa0390350c08a210e87bdfe644698e99cd92
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-27 18:59:29 -0500

    include/robot.hpp: Fix formatting

--- include/robot.hpp
+++ include/robot.hpp
@@ -1,8 +1,8 @@
 /** Copyright 2023 Osprey Robotics - UNF
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*/
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ */
 
 #ifndef OSPREY_ROBOTICS__ROBOT_HPP
 #define OSPREY_ROBOTICS__ROBOT_HPP
@@ -11,43 +11,43 @@
 
 #include "segment.hpp"
 
-extern const char* CLASS_NAME;
+extern const char *CLASS_NAME;
 
 namespace osprey_robotics
 {
     /**
      * @brief Class that represents the total robot, all joints, sensors, etc.
      */
-	class Robot
-	{
-		private:
+    class Robot
+    {
+        private:
 
-		public:
+        public:
             /**
              * @brief Construct a new Robot object, creates a new usb context
              *        and interface, then loads all USB devices, and initializes
              *        all joints, sensors, etc
              */
-			Robot();
+            Robot();
 
             /**
              * @brief Destroy the Robot object, empty/unused
              */
-			~Robot();
+            ~Robot();
 
             /**
              * @brief Represents the robot
              */
-			Segment<4> base;
+            Segment<4> base;
 
             /**
              * @brief Get a Joint object by name
-             * 
+             *
              * @param jointName name of the joint
              * @return Joint
              */
-			Joint getJoint(std::string jointName);
-	};
+            Joint getJoint(std::string jointName);
+    };
 }
 
 #endif
