d1d24a028de47405fd465a55955ab0ada1b3ea1b
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-27 18:57:03 -0500

    src/robot.cpp: Fix formatting

--- src/robot.cpp
+++ src/robot.cpp
@@ -10,43 +10,43 @@
 
 namespace osprey_robotics
 {
-	Robot::Robot()
-	{
+    Robot::Robot()
+    {
         // shared USB interface and devices
         USB usb = USB();
         usb.loadDevices(MOTOR_USB_VID, MOTOR_USB_PID);
 
-		// robot base
-		base.joints[0] = Joint(1, SERIAL_FRONT_LEFT_1, ACTUATOR_TYPE_MOTOR);
-		base.joints[0].name = "front_left_wheel_joint";
+        // robot base
+        base.joints[0] = Joint(1, SERIAL_FRONT_LEFT_1, ACTUATOR_TYPE_MOTOR);
+        base.joints[0].name = "front_left_wheel_joint";
         base.joints[0].usb = &usb;
-		base.joints[1] = Joint(2, SERIAL_FRONT_RIGHT_3, ACTUATOR_TYPE_MOTOR);
-		base.joints[1].name = "front_right_wheel_joint";
+        base.joints[1] = Joint(2, SERIAL_FRONT_RIGHT_3, ACTUATOR_TYPE_MOTOR);
+        base.joints[1].name = "front_right_wheel_joint";
         base.joints[1].usb = &usb;
-		base.joints[2] = Joint(3, SERIAL_BACK_LEFT_2, ACTUATOR_TYPE_MOTOR);
-		base.joints[2].name = "rear_left_wheel_joint";
+        base.joints[2] = Joint(3, SERIAL_BACK_LEFT_2, ACTUATOR_TYPE_MOTOR);
+        base.joints[2].name = "rear_left_wheel_joint";
         base.joints[2].usb = &usb;
-		base.joints[3] = Joint(4, SERIAL_BACK_RIGHT_4, ACTUATOR_TYPE_MOTOR);
-		base.joints[3].name = "rear_right_wheel_joint";
+        base.joints[3] = Joint(4, SERIAL_BACK_RIGHT_4, ACTUATOR_TYPE_MOTOR);
+        base.joints[3].name = "rear_right_wheel_joint";
         base.joints[3].usb = &usb;
-	}
+    }
 
-	Robot::~Robot()
-	{
+    Robot::~Robot()
+    {
 
-	}
+    }
 
-	Joint Robot::getJoint(std::string jointName)
-	{
-		int numJointsBase = sizeof(base.joints) / sizeof(base.joints[0]);
-		for (int i = 0; i < numJointsBase; i++)
-		{
-			if (base.joints[i].name == jointName)
-			{
-				return base.joints[i];
-			}
-		}
+    Joint Robot::getJoint(std::string jointName)
+    {
+        int numJointsBase = sizeof(base.joints) / sizeof(base.joints[0]);
+        for (int i = 0; i < numJointsBase; i++)
+        {
+            if (base.joints[i].name == jointName)
+            {
+                return base.joints[i];
+            }
+        }
 
-		throw std::runtime_error("Could not find joint with name " + jointName);
-	}
+        throw std::runtime_error("Could not find joint with name " + jointName);
+    }
 }
