903c28b2cc4386954eb79ca185ae3cd140bc109c
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-27 18:30:51 -0500

    include/robot.hpp: Initial header for Robot class

---
+++ include/robot.hpp
@@ -0,0 +1,53 @@
+/** Copyright 2023 Osprey Robotics - UNF
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*/
+
+#ifndef OSPREY_ROBOTICS__ROBOT_HPP
+#define OSPREY_ROBOTICS__ROBOT_HPP
+
+#include <string>
+
+#include "segment.hpp"
+
+extern const char* CLASS_NAME;
+
+namespace osprey_robotics
+{
+    /**
+     * @brief Class that represents the total robot, all joints, sensors, etc.
+     */
+	class Robot
+	{
+		private:
+
+		public:
+            /**
+             * @brief Construct a new Robot object, creates a new usb context
+             *        and interface, then loads all USB devices, and initializes
+             *        all joints, sensors, etc
+             */
+			Robot();
+
+            /**
+             * @brief Destroy the Robot object, empty/unused
+             */
+			~Robot();
+
+            /**
+             * @brief Represents the robot
+             */
+			Segment<4> base;
+
+            /**
+             * @brief Get a Joint object by name
+             * 
+             * @param jointName name of the joint
+             * @return Joint
+             */
+			Joint getJoint(std::string jointName);
+	};
+}
+
+#endif
