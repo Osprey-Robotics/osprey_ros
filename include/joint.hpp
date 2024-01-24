19da15914cac5c5e6bddca483694389970839db8
William L. Thomson Jr. <wlt@o-sinc.com>
2024-01-23 22:20:54 -0500

    include/joint.hpp: Remove relocated usb serial # vars, now in robot.hpp

--- include/joint.hpp
+++ include/joint.hpp
@@ -24,15 +24,6 @@
 #define MOTOR_USB_VID 0x0483
 #define MOTOR_USB_ENDPOINT 0x02
 
-// motor USB serial numbers
-const std::string SERIAL_FRONT_LEFT_1 = "206D33614D43";
-const std::string SERIAL_BACK_LEFT_2 = "2061376C4243";
-const std::string SERIAL_FRONT_RIGHT_3 = "206B336B4E55";
-const std::string SERIAL_BACK_RIGHT_4 = "205A336B4E55";
-const std::string SERIAL_LADDER_DIG = "206A33544D43";
-const std::string SERIAL_LADDER_LIFT = "206C395A5543";
-const std::string SERIAL_DEPOSITION = "205D39515543";
-
 // values in joint.cpp
 // speed of motors range -0.50 to 0.50 big endian
 extern unsigned char SPEED[MOTOR_USB_LEN];
