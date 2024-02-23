c620ab17bd248850a86071aa8c19af7f9feeffa4
William L. Thomson Jr. <wlt@o-sinc.com>
2024-02-22 23:43:07 -0500

    launch2/ign_gazebo.launch.py: Remove unused import

--- launch2/ign_gazebo.launch.py
+++ launch2/ign_gazebo.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, OpaqueFunction, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_context import LaunchContext
 from launch.launch_description_sources import PythonLaunchDescriptionSource
