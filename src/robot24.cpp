4cc3efd36df45ee3450991a7baec5fcab0281f8a
William L. Thomson Jr. <wlt@o-sinc.com>
2024-03-31 23:43:32 -0400

    src/robot24.cpp: Intial code for 2024 robot hardware, wheels only atm

---
+++ src/robot24.cpp
@@ -0,0 +1,56 @@
+/** Copyright 2024 Osprey Robotics - UNF
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*/
+
+#include <stdexcept>
+
+#include "robot.hpp"
+
+namespace osprey_robotics
+{
+    Robot::Robot()
+    {
+        // shared USB interface and devices
+        static USB usb = USB();
+        usb.loadDevices(MOTOR_USB_VID, MOTOR_USB_PID);
+
+        // robot base
+        base.joints[0] = Joint(1, SERIAL_FRONT_LEFT_1, ACTUATOR_TYPE_MOTOR);
+        base.joints[0].multiplier = SPEED_MULTIPLIER_WHEELS;
+        base.joints[0].name = "front_left_wheel_joint";
+        base.joints[0].usb = &usb;
+        base.joints[1] = Joint(2, SERIAL_FRONT_RIGHT_3, ACTUATOR_TYPE_MOTOR);
+        base.joints[1].multiplier = SPEED_MULTIPLIER_WHEELS;
+        base.joints[1].name = "front_right_wheel_joint";
+        base.joints[1].usb = &usb;
+        base.joints[2] = Joint(3, SERIAL_BACK_LEFT_2, ACTUATOR_TYPE_MOTOR);
+        base.joints[2].multiplier = SPEED_MULTIPLIER_WHEELS;
+        base.joints[2].name = "rear_left_wheel_joint";
+        base.joints[2].usb = &usb;
+        base.joints[3] = Joint(4, SERIAL_BACK_RIGHT_4, ACTUATOR_TYPE_MOTOR);
+        base.joints[3].multiplier = SPEED_MULTIPLIER_WHEELS;
+        base.joints[3].name = "rear_right_wheel_joint";
+        base.joints[3].usb = &usb;
+    }
+
+    Robot::~Robot()
+    {
+
+    }
+
+    Joint Robot::getJoint(std::string jointName)
+    {
+        int numJointsBase = sizeof(base.joints) / sizeof(base.joints[0]);
+        for (int i = 0; i < numJointsBase; i++)
+        {
+            if (base.joints[i].name == jointName)
+            {
+                return base.joints[i];
+            }
+        }
+
+        throw std::runtime_error("Could not find joint with name " + jointName);
+    }
+}
